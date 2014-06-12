5a2c4b4fa3c0a17c289a9268e27221a9764de023
futureinternetlab <alastair.little@me.com>
2014-06-12 10:24:56 +0100

    Adding jQuery

---
+++ redirect_script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+	// First we need to redirect to GWIQ, to read the 'gwid' value from the cookie
+
+	// Then we'll redirect back to futureinternetlab.com, passing the 'gwid' as a variable
+
+	// Next we'll append the 'gwid' to the end of the survey link
+
+	var gwid = "xyz"
+	var iframeURL = "<iframe src='https://futureinternetlab.typeform.com/to/ACu24s?gwid=" + gwid + "'" + "scrolling='no'></iframe>"
+	
+	//Finally we'll load the iframe into the body element, with the full survey URL including 'gwid' used as the iframe source
+	$('body').html(iframeURL);
+
+});
